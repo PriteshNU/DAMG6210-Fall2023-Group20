97728573e6df81570fd41a3f6514c9ee50d2421c
nimje.p@northeastern.edu <nimje.p@northeastern.edu>
2024-04-06 23:25:19 -0400

    Updated views

--- Scripts/P4_Views.sql
+++ Scripts/P4_Views.sql
@@ -3,7 +3,8 @@ USE CMS;
 --------------------------------------------------------------------------------------------------------------------------------
 -- View to get summary of service requests
 GO
-CREATE VIEW ServiceRequestSummary AS
+CREATE VIEW ServiceRequestSummary 
+AS
 SELECT
     sr.ServiceRequestID,
     sr.[Description],
@@ -20,4 +21,58 @@ FROM
     LEFT JOIN Resident r ON sr.ResidentID = r.ResidentID
     LEFT JOIN Staff s ON sr.StaffAssignedID = s.StaffID;
 GO
+--------------------------------------------------------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------------------------------------------------------
+CREATE VIEW AmenityBookingsDetails
+AS
+SELECT
+    ab.AmenityBookingID,
+    a.Name AS AmenityName,
+    r.ResidentID,
+    r.FirstName + ' ' + r.LastName AS ResidentName,
+    r.ContactNumber,
+    ab.BookingDate,
+    CONVERT(VARCHAR, ab.StartTime, 108) + ' - ' + CONVERT(VARCHAR, ab.EndTime, 108) AS BookingTime,
+    ab.NumOfAttendees,
+    CASE 
+        WHEN ab.BookingFee > 0 THEN CAST(ab.BookingFee AS VARCHAR) + ' $'
+        ELSE 'Free'
+    END AS BookingFee,
+    ab.NumOfAttendees
+FROM
+    AmenityBooking ab
+JOIN
+    Amenity a ON ab.AmenityID = a.AmenityID
+JOIN
+    Resident r ON ab.ResidentID = r.ResidentID
+WHERE
+    ab.BookingDate >= CAST(GETDATE() AS DATE);
+--------------------------------------------------------------------------------------------------------------------------------
+
+
+--------------------------------------------------------------------------------------------------------------------------------
+CREATE VIEW OccupiedParkingDetails
+AS
+SELECT
+    ps.ParkingSlotID,
+    CASE 
+        WHEN r.ResidentID IS NOT NULL THEN 'Resident'
+        WHEN v.VisitorID IS NOT NULL THEN 'Visitor'
+        ELSE 'Unknown'
+    END AS OccupantType,
+    COALESCE(r.ResidentID, v.VisitorID) AS OccupantID,
+    COALESCE(r.FirstName + ' ' + r.LastName, v.FirstName + ' ' + v.LastName) AS OccupantName,
+    COALESCE(r.ContactNumber, v.ContactNumber) AS ContactNumber,
+    COALESCE(vehicle.LicensePlate, 'N/A') AS VehicleLicense
+FROM
+    ParkingSlot ps
+LEFT JOIN
+    Resident r ON ps.VehicleID = r.ResidentID
+LEFT JOIN
+    Visitor v ON ps.VehicleID = v.VisitorID
+LEFT JOIN
+    Vehicle vehicle ON ps.VehicleID = vehicle.VehicleID
+WHERE
+    ps.Status = 'Occupied';
 --------------------------------------------------------------------------------------------------------------------------------
